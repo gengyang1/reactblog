ad75a2eb86d8a7e311f823bc43f5a0888ac9a475
gengyang1 <1169957449@qq.com>
2020-11-19 09:18:14 +0800

    a

--- my-app/components/footer.jsx
+++ my-app/components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = ()=>(
     <div className="footer-div">
         <div>系统由 Nextjs+ Egg + Ant Desgin驱动 </div>
-        <div>gyblog.online</div>
+        <div>备案许可证编号为：津ICP备20000237号-2</div>
     </div>
 )
 
